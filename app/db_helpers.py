38e31b46171fcaec771524cdb37fbd491308d17f
seanald <sean_m_fitzgerald@tutanota.com>
2020-05-05 21:14:31 -0700

    Added db_helpers, cards.db, basics to routes.py

---
+++ app/db_helpers.py
@@ -0,0 +1,90 @@
+import sqlite3, json, secrets, 
+
+def conn():
+    return sqlite3.connect('cards.db')
+
+def rowConn():
+    conn = sqlite3.connect('cards.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def dbQry(sql, *parameters):
+    conn = conn()
+    c = conn.connect()
+    return c.execute(sql, parameters)
+
+def dbIsPlayer(player_id):
+    record = curs().execute('SELECT player_id FROM players WHERE player_id=?',
+        player_id).fetchone()
+    return bool(player)
+
+def dbAddPlayer(player_name):
+    conn = conn()
+    c = conn.connect()
+    player_ids = []
+    for record in c.execute(
+            'SELECT player_id FROM players').fetchall():
+        player_ids.append(record[0])
+
+    player_id = secrets.token_urlsafe(32)
+    while player_id in player_ids:
+        player_id = secrets.token_urlsafe(32)
+
+    c.execute(
+            'INSERT INTO players (player_id, player_name, last_login)\
+                    VALUES (?,?,?)',
+            player_id, player_name, time.time()).close()
+
+    conn.commit().close()
+    return player_id
+
+def dbUpdatePlayer(player_id, player_name=False, last_login=False):
+    conn = conn()
+    c = conn.connect()
+
+    if player_name:
+        c.execute('UPDATE players SET player_name=? WHERE player_id=?',
+                player_name, player_id)
+    if last_login:
+        c.execute('UPDATE players SET last_login=? WHERE player_id=?',
+                last_login, player_id)
+
+    conn.commit().close()
+
+def dbRmPlayer(player_id):
+    conn = conn()
+    c = conn.connect()
+    c.execute('DELETE FROM players WHERE player_id=?', player_id)
+    conn.commit().close()
+
+def dbAddGame(game_name, host_player_id):
+    conn = conn()
+    c = conn.connect()
+    game_ids = []
+    for record in c.execute(
+            'SELECT game_id FROM players').fetchall():
+        game_ids.append(record[0])
+
+    game_id = secrets.token_urlsafe(32)
+    while game_id in game_ids:
+        game_id = secrets.token_urlsafe(32)
+
+    c.execute(
+            'INSERT INTO games (game_id, game_name, host) VALUES (?,?,?)',
+            game_id, game_name, host_player_id)
+
+    conn.commit().close()
+
+    return game_id
+
+def dbRmGame(game_id):
+    conn = conn()
+    c = conn.connect()
+    c.execute('DELETE FROM games WHERE game_id=?', game_id).close()
+    conn.commit().close()
+
+def addSession(player_id, game_id):
+    conn = conn()
+    c = conn.connect()
+    c.execute('INSERT INTO sessions (player_id, game_id) VALUES (?,?)')
+    conn.commit().close()
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,50 @@
 from app import app
 from flask import (render_template, render_template_string, request,
-        session, send_from_directory, send_file)
-import os, sqlite3
+        session, send_from_directory, send_file, url_for)
+import os, sqlite3, json, re
+
+from db_helpers import *
+from cards_helpers import *
 
 with open('secretkey') as keyfile:
     app.secret_key = keyfile.readline()
     keyfile.close()
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if session and session['player_id']:
+        return session['player_id']
+    elif request.method == 'POST' and request.form['player_name']:
+            player_id = dbAddPlayer(request.form['player_name'])
+            session['player_id'] = player_id
+            return 'SUCCESS\n' + player_id
+    else:
+        return 'FAIL'
 
-@app.route('/')
-def test():
-    return 'test'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and dbIsPlayer(request.form['player_id']):
+        session['player_id'] = request.form['player_id']
+        dbUpdatePlayer(request.form['player_id'], last_login=time.time())
+        return 'SUCCESS\n'
+    else:
+        return 'FAIL'
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST' and request.form['player_id']:
+        dbRmPlayer(request.form['player_id'])
+
+@app.route('/create', methods=['GET', 'POST'])
+def createGame():
+    if request.method == 'POST':
+        game_name = request.form['game_name']
+        if(session and session['player_id']
+                and dbIsPlayer(session['player_id'])):
+            player_id = request.form['player_id']
+            game_id = dbAddGame(game_name, player_id)
+            return 'SUCCESS\n' + game_id
+
+
+@app.route('/games')
+def games():
