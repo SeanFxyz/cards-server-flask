651d8ff67970e880663e1ae98454e309b8c8f658
seanald <sean_m_fitzgerald@tutanota.com>
2020-05-10 10:31:22 -0700

    Added fields, db functions, etc.

--- app/db_helpers.py
+++ app/db_helpers.py
@@ -1,10 +1,11 @@
 import sqlite3, json, secrets
+from hashlib import sha512
 
 def conn():
-    return sqlite3.connect('cards.db')
+    return sqlite3.connect("cards.db")
 
 def rowConn():
-    conn = sqlite3.connect('cards.db')
+    conn = sqlite3.connect("cards.db")
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -23,7 +24,7 @@ def dbRowQry(sql, *parameters):
     return qry
 
 def dbIsPlayer(player_id):
-    record = curs().execute('SELECT player_id FROM players WHERE player_id=?',
+    record = curs().execute("SELECT player_id FROM players WHERE player_id=?",
         player_id).fetchone()
     return bool(player)
 
@@ -32,7 +33,7 @@ def dbAddPlayer(player_name):
     c = conn.cursor()
     player_ids = []
     for record in c.execute(
-            'SELECT player_id FROM players').fetchall():
+            "SELECT player_id FROM players").fetchall():
         player_ids.append(record[0])
 
     player_id = secrets.token_urlsafe(32)
@@ -40,8 +41,8 @@ def dbAddPlayer(player_name):
         player_id = secrets.token_urlsafe(32)
 
     c.execute(
-            'INSERT INTO players (player_id, player_name, last_login)\
-                    VALUES (?,?,?)',
+            "INSERT INTO players (player_id, player_name, last_login) \
+                    VALUES (?,?,?)",
             player_id, player_name, time.time())
 
     conn.commit().close()
@@ -52,10 +53,10 @@ def dbUpdatePlayer(player_id, player_name=False, last_login=False):
     c = conn.cursor()
 
     if player_name:
-        c.execute('UPDATE players SET player_name=? WHERE player_id=?',
+        c.execute("UPDATE players SET player_name=? WHERE player_id=?",
                 player_name, player_id)
     if last_login:
-        c.execute('UPDATE players SET last_login=? WHERE player_id=?',
+        c.execute("UPDATE players SET last_login=? WHERE player_id=?",
                 last_login, player_id)
 
     conn.commit().close()
@@ -63,24 +64,26 @@ def dbUpdatePlayer(player_id, player_name=False, last_login=False):
 def dbRmPlayer(player_id):
     conn = conn()
     c = conn.cursor()
-    c.execute('DELETE FROM players WHERE player_id=?', player_id)
+    c.execute("DELETE FROM players WHERE player_id=?", player_id)
+    c.execute("DELETE FROM sessions WHERE player_id=?", player_id)
     conn.commit().close()
 
-def dbAddGame(game_name, host_player_id):
+def dbAddGame(game_name, host_player_id, passwd="", hidden=0):
     conn = conn()
     c = conn.cursor()
     game_ids = []
     for record in c.execute(
-            'SELECT game_id FROM players').fetchall():
+            "SELECT game_id FROM players").fetchall():
         game_ids.append(record[0])
 
-    game_id = secrets.token_urlsafe(32)
+    game_id = secrets.token_hex(4)
     while game_id in game_ids:
-        game_id = secrets.token_urlsafe(32)
+        game_id = secrets.token_hex(4)
 
     c.execute(
-            'INSERT INTO games (game_id, game_name, host) VALUES (?,?,?)',
-            game_id, game_name, host_player_id)
+            "INSERT INTO games (game_id, game_name, host, hidden) \
+                    VALUES (?,?,?,?)",
+            game_id, game_name, host_player_id, hidden)
 
     conn.commit().close()
 
@@ -89,11 +92,34 @@ def dbAddGame(game_name, host_player_id):
 def dbRmGame(game_id):
     conn = conn()
     c = conn.cursor()
-    c.execute('DELETE FROM games WHERE game_id=?', game_id).close()
+    c.execute("DELETE FROM games WHERE game_id=?", game_id).close()
     conn.commit().close()
 
-def addSession(player_id, game_id):
+def dbAddSession(player_id, game_id):
     conn = conn()
     c = conn.cursor()
-    c.execute('INSERT INTO sessions (player_id, game_id) VALUES (?,?)')
+    c.execute("INSERT INTO sessions (player_id, game_id) VALUES (?,?)")
     conn.commit().close()
+
+def dbIsSession(game_id, player_id):
+    conn = rowConn()
+    c = conn.cursor()
+    row = c.execute("SELECT * FROM sessions WHERE game_id=? AND player_id=?",
+            game_id, player_id).fetchone()
+    conn.close()
+    return bool(row)
+
+def dbCheckPass(game_id, passwd):
+    row = dbRowQry("SELECT (game_id, passwd_hash) FROM games WHERE game_id=?",
+            game_id).fetchone()
+
+    if row["game_id"] == game_id:
+        passwd_hash = row["passwd_hash"]
+        if passwd_hash:
+            if(passwd_hash == 
+                    sha512(str(passwd + game_id).encode()).hexdigest()):
+                return True
+
+            return False
+        else:
+            return True
--- app/routes.py
+++ app/routes.py
@@ -6,41 +6,67 @@ import os, sqlite3, json, re
 from app.db_helpers import *
 from app.cards_helpers import *
 
-with open('secretkey') as keyfile:
+with open("secretkey") as keyfile:
     app.secret_key = keyfile.readline()
     keyfile.close()
 
-@app.route('/login', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST' and request.form['player_name']:
-            player_id = dbAddPlayer(request.form['player_name'])
-            return 'SUCCESS\n' + player_id
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST" and request.form["player_name"]:
+            player_id = dbAddPlayer(request.form["player_name"])
+            return "SUCCESS\n" + player_id
     else:
-        return 'FAIL'
+        return "FAIL"
 
-@app.route('/logout', methods=['POST'])
+@app.route("/logout", methods=["POST"])
 def logout():
-    if request.method == 'POST' and request.form['player_id']:
-        dbRmPlayer(request.form['player_id'])
-        return 'SUCCESS'
+    if request.method == "POST":
+        dbRmPlayer(request.form["player_id"])
+        return "SUCCESS"
     else:
-        return 'FAIL'
+        return "FAIL"
 
-@app.route('/create', methods=['POST'])
+@app.route("/create", methods=["POST"])
 def createGame():
-    if request.method == 'POST':
-        game_name = request.form['game_name']
-        player_id = request.form['player_id']
+    if request.method == "POST":
+        game_name = request.form["game_name"]
+        player_id = request.form["player_id"]
+        hidden = int(reqeust.form["hidden"])
         if dbIsPlayer(player_id):
-            game_id = dbAddGame(game_name, player_id)
-            return 'SUCCESS\n' + game_id
+            game_id = dbAddGame(game_name, player_id, hidden)
+            return "SUCCESS\n" + game_id
         else:
-            return 'FAIL\nInvalid player ID'
+            return "FAIL\nInvalid player ID"
 
-@app.route('/qry', methods=['POST'])
+@app.route("/join", methods=["POST"])
+def joinGame():
+    if request.method == "POST":
+        game_id = request.form["game_id"]
+        player_id = request.form["player_id"]
+        passwd = request.form["password"]
+        if dbCheckPass(game_id, passwd):
+            dbAddSession(player_id, game_id)
+            return "SUCCESS\n"
+        return "FAIL"
+
+@app.route("/qry", methods=["POST"])
 def qryGame():
-    if request.method == 'POST':
-        game_id = request.form['game_id']
-        qry = dict(dbRowQry('SELECT * FROM games WHERE game_id=?', game_id)[0])
-        qry_json = json.dumps(qry)
-        return 'SUCCESS\n' + qry_json
+    if request.method == "POST":
+        game_id = request.form["game_id"]
+        player_id = request.form["player_id"]
+        if dbIsSession(game_id, player_id):
+            qry = dict(dbRowQry("SELECT * FROM games WHERE game_id=?", game_id)[0])
+            playerQry = dbQry("SELECT player_id FROM sessions WHERE game_id=?",
+                    game_id)
+            # TODO: convert playerQry to plain list of players
+            qry_json = json.dumps(qry)
+            return "SUCCESS\n" + qry_json
+        
+        return "FAIL\n"
+
+@app.route("/games")
+def getGames():
+    qry = (dbRowQry("SELECT (game_id, game_name, host, state) FROM games \
+                        WHERE hidden=?", 0))
+    qry_json = json.dumps([dict(row) for row in qry])
+    return "SUCCESS\n" + qry_json
